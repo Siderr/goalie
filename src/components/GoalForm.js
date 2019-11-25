f5510aecd2fccc4396dbe949057053ed72635f42
TadasBit <tadas.bit@gmail.com>
2019-11-25 14:08:43 +0200

    Added form using react-hook-form.

--- src/components/GoalForm.js
+++ src/components/GoalForm.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import useForm from 'react-hook-form';
 
 const GoalForm = () => {
@@ -6,16 +6,47 @@ const GoalForm = () => {
     const onSubmit = data => console.log(data);
     console.log(errors);
 
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" placeholder="Name" name="Name" ref={register({required: true, max: 255, min: 10})} />
-      <textarea name="Description" ref={register({min: 0, maxLength: 3000})} />
-      <select name="Origin" ref={register({ required: true })}>
-      </select>
+    useEffect(()=> {
+        
 
-      <input type="submit" />
-    </form>
-  )
+
+    },[])
+
+
+    return (
+        <div className="container">
+            <div className="notification">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="field">
+                        <label className="label">Name</label>
+                        <div className="control">
+                            <input className="input" type="text" placeholder="Goal" name="Name" ref={register({ required: true, max: 255, min: 10 })} />
+                        </div>
+                    </div>
+                    <div className="field">
+                        <label className="label">Description</label>
+                        <div className="control">
+                            <textarea className="textarea" name="Description" placeholder="Insert some description text for your goal." ref={register({ min: 0, maxLength: 3000 })} />
+                        </div>
+                    </div>
+                    <div className="field">
+                        <label className="label">Origin</label>
+                        <div className="control">
+                            <div className="select">
+                                <select name="Origin" ref={register({ required: false })}>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="field">
+                        <div className="control">
+                            <button className="button is-link">Submit</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default GoalForm
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import './index.css';
+import './../node_modules/bulma/bulma.sass';
+// import App from './App';
 import GoalForm from './components/GoalForm'
 import * as serviceWorker from './serviceWorker';
 
